24284190f527712be567a8432a075a8e6a955255
Deji Adegbite <d2alphame@yahoo.com>
2026-05-02 19:22:14 +0100

    Defined items to be used by the fabric in vixen.hpp file

--- src/vixen.hpp
+++ src/vixen.hpp
@@ -1,1 +1,32 @@
-class enum MemoryRegionMask
+#pragma once
+
+// Enum defining the type of transaction.
+enum class TransactionType : bool {
+    READ = false,
+    WRITE = true
+};
+
+
+// Enum defining the Data Width Mask
+enum class DataWidthMask : uint64_t {
+    BYTE  =               0xFF,  // 8 bits
+    WORD  =             0xFFFF,  // 16 bits
+    DWORD =         0xFFFFFFFF,  // 32 bits
+    QWORD = 0xFFFFFFFFFFFFFFFF   // 64 bits
+};
+
+
+// Struct definining a memory transaction.
+struct MemoryTransaction {
+    uint64_t        address;
+    uint64_t        data;
+    DataWidthMask   data_width_mask; // Mask indicating the width of the data to read/write
+    TransactionType control;  // READ for read, WRITE for write.
+};
+
+
+// Fabric writers implement this namespace.
+namespace Fabric {
+    void     init(uint64_t rom_base, uint64_t rom_size);
+    uint64_t execute(MemoryTransaction transaction);
+}
