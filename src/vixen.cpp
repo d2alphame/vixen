44d5f86022a890e0809091bcafa7fc730e23cd6d
Deji Adegbite <d2alphame@yahoo.com>
2026-05-06 01:06:48 +0100

    Work in progresss

--- src/vixen.cpp
+++ src/vixen.cpp
@@ -1,8 +1,70 @@
 // The main program for the vixen emulator
 
+#include <iostream>
 #include <cstdint>
+#include <filesystem>
+
 #include "vixen.hpp"
 
-int main() {
+#define MAX_ROM_SIZE 0x200000 // 2 MB
 
+int main(int argc, char* argv[]) {
+    
+    enum Result : int {
+        Success                  = 0,
+        InvalidSubcommand        = 1,
+        InvalidFilename          = 2,
+        FileNotFoundOrNotRegular = 3,
+        FileTooLarge             = 4,
+    };
+    
+    // The user passes in the name of the binary file to use as a ROM when starting the emulator.
+    std::string subcommand = argv[1];
+
+    // If the subcommand is "load-rom", we expect the name of the ROM binary file to follow
+    if(subcommand == "load-rom") {
+        // std::string rom_file = argv[2];
+        std::filesystem::path rom_file = argv[2];
+
+        // Check that the ROM file string is not empty
+        if(rom_file.empty()) {
+            std::cerr << "Error: ROM file name is empty. Enter a valid ROM file name." << std::endl;
+            return Result::InvalidFilename;
+        }
+
+        // Check if the file exists and is a regular file
+        if(!std::filesystem::exists(rom_file) || !std::filesystem::is_regular_file(rom_file)) {
+            std::cerr << "Error: ROM file does not exist or is not a regular file. Enter a valid ROM file name." << std::endl;
+            return Result::FileNotFoundOrNotRegular;
+        }
+
+        // Check if the file is too large
+        if(std::filesystem::file_size(rom_file) > MAX_ROM_SIZE) { // 2 MB limit for ROM size
+            std::cerr << "Error: ROM file is too large. Maximum allowed size is " << MAX_ROM_SIZE << " bytes." << std::endl;
+            return Result::FileTooLarge;
+        }
+    }
+
+    return Result::Success;
+}
+
+__attribute__((weak)) void Fabric::init(uint64_t rom_base, uint64_t rom_size) {
+    // Default implementation of the Fabric::init function.
+    // This can be overridden by the user to provide custom initialization logic for the fabric.
+}
+
+__attribute__((weak)) uint64_t Fabric::execute(MemoryTransaction transaction) {
+    // Default implementation of the Fabric::execute function.
+    // This can be overridden by the user to provide custom execution logic for the fabric.
+    return 0;
+}
+
+__attribute__((weak)) void Cpu::init(uint64_t reset_vector) {
+    // Default implementation of the Cpu::init function.
+    // This can be overridden by the user to provide custom initialization logic for the CPU.
+}
+
+__attribute__((weak)) void Cpu::power_on() {
+    // Default implementation of the Cpu::power_on function.
+    // This can be overridden by the user to provide custom power-on logic for the CPU.
 }
